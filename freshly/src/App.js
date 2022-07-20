718c92cfeba8a693d6cf1fd29b8f76232a1dc0a1
Rajendra Nali <80375569+rajendranali@users.noreply.github.com>
2022-07-20 08:07:22 +0530

    nav

--- freshly/src/App.js
+++ freshly/src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
-
+import Navbar from './Navbar/Navbar';
 import {ChakraProvider} from "@chakra-ui/react"
 import { Children } from 'react';
 function App() {
   return (
     <div className="App">
       <ChakraProvider>
-     {Children}
+     <Navbar/>
       </ChakraProvider>
       
     </div>
