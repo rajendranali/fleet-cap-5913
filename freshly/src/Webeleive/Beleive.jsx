d1ee7949f7f0e1e17306653a63a24bcc83fabc1e
Rajendra Nali <80375569+rajendranali@users.noreply.github.com>
2022-07-21 22:08:24 +0530

    updated

--- freshly/src/Webeleive/Beleive.jsx
+++ freshly/src/Webeleive/Beleive.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import {Box,Image,Heading,Text,Button} from "@chakra-ui/react"
+import {Box,Image,Heading,Text,Button,Input} from "@chakra-ui/react"
 import styles from "./beleive.module.css"
 const Beleive = () => {
   return (
+    <>
     <Box className={styles.beleive}>
         <Box className={styles.box1}>
            <Heading textAlign="left" marginTop="50px">We believe eating right should be easy for everyone.</Heading>
@@ -12,8 +13,27 @@ const Beleive = () => {
         <Box w="60%">
          <Image  w="100%"src="https://assets-global.website-files.com/5d03b4e130118314af624b20/61f1275059fd961964f53287_22_Homepage_EatingBetterEasy_BG_866x708.jpg"/>
         </Box>
-
+        
+        
     </Box>
+    <Box className={styles.beleive} marginTop="120px">
+    <Box w="60%">
+         <Image  w="100%"src="https://assets-global.website-files.com/5d03b4e130118314af624b20/61f1274f0c096f517bb89ed5_22_Homepage_HealthyMealsDelivered_866x708.jpg"/>
+        </Box>
+        <Box className={styles.box2}>
+           <Heading textAlign="left" marginTop="50px">Healthy Meals Delivered Weekly</Heading>
+          <Input type="email" placeholder='rajendra@gmail.com'  marginTop="50px"/>
+          <br/>
+          <br />
+          <Input type="text" placeholder='ZIP'/>
+           <Button marginRight="800px" marginTop="80px" w="400px" h="50px" bg="blue"color="white" borderRadius="0px">Get Started</Button>
+        </Box>
+       
+        
+        
+    </Box>
+    
+    </>
   )
 }
 
